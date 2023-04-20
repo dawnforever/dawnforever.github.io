62d69de1485371b2ae8337f1f874da2f78fe9ab3
dawnforever <zlm15265152125@outlook.com>
2023-04-20 14:43:31 +0800

    Site updated: 2023-04-20 14:43:29

---
+++ dist/music.js
@@ -0,0 +1,67 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+	  {
+        name: 'The Other Side Of Paradise',
+        artist: 'Glass Animals',
+        url: 'http://music.163.com/song/media/outer/url?id=427416520.mp3',
+        cover: '/images/music1.jpg',
+      },
+      {
+        name: "精卫",
+        artist: '30年前，50年后',
+        url: 'http://music.163.com/song/media/outer/url?id=1951069525.mp3',
+        cover: '/images/music2.jpg',
+      },
+      {
+        name: '雪 Distance',
+        artist: 'Capper,罗言',
+        url: 'http://music.163.com/song/media/outer/url?id=2026224214.mp3',
+        cover: '/images/zjsam.jpg',
+      },
+      {
+        name: '给外婆的一封信',
+        artist: 'Moseee,少数π',
+        url: 'http://music.163.com/song/media/outer/url?id=2006570469.mp3',
+        cover: '/images/music4.jpg',
+      },
+	  {
+        name: '在你的身边 ',
+        artist: '盛哲',
+        url: 'http://music.163.com/song/media/outer/url?id=475479888.mp3',
+        cover: '/images/music5.jpg',
+      },
+	  {
+        name: 'Колыбельн',
+        artist: 'Rauf & Faik',
+        url: 'http://music.163.com/song/media/outer/url?id=1852108940.mp3',
+        cover: '/images/music6.jpg',
+      },
+	  {
+        name: 'это ли счастье?',
+        artist: 'Rauf & Faik',
+        url: 'http://music.163.com/song/media/outer/url?id=1854933676.mp3',
+        cover: '/images/music7.jpg',
+      },
+	  {
+        name: 'Head In The Clouds',
+        artist: 'Hayd',
+        url: 'http://music.163.com/song/media/outer/url?id=1878812258.mp3',
+        cover: '/images/music8.jpg',
+      },
+	  {
+        name: '初恋',
+        artist: '回春丹',
+        url: 'http://music.163.com/song/media/outer/url?id=1873049720.mp3',
+        cover: '/images/music9.jpg',
+      },
+       {
+        name: '任意门',
+        artist: '五月天',
+        url: 'http://music.163.com/song/media/outer/url?id=1367142413.mp3',
+        cover: '/images/music10.jpg',
+      }
+    ]
+});
